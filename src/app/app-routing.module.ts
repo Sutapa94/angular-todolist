2a6bc3b79b5b4a0be2554400110d87061732b339
Sutapa94 <sutapa.banerjee1994@gmail.com>
2022-07-02 18:45:14 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './MyComponents/about/about.component';
+import { HomeComponent } from './MyComponents/home/home.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:TodosComponent},
+  {path:'about',component:AboutComponent},
+  {path:'home',component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
